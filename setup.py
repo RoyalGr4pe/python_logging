e5426491ee98e2a34ccca33f1a84dc7d796c9a1a
RoyalGrape <109209370+RoyalGr4pe@users.noreply.github.com>
2023-03-25 21:08:27 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name='python_logging',
+    version='0.0.1',
+    description='Python logging tool for .log files',
+    url='https://github.com/RoyalGr4pe/python_logging',
+    author="Royal Gr4pe",
+    author_email="njames.programming@gmail.com",
+    packages=setuptools.find_packages(),
+    install_requires=['loguru'],
+    long_description="Python logging tool for .log file"
+)
